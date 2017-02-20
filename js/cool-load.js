31e80c58fd41885ebc95e3d39fca256daeaab227
Casey Erdmann <casey@injecti0n.org>
2017-02-20 15:49:54 -0500

    added local storage checker to make loading better
    if user allows local storage, it stores the localstorage object so they don't see the loading screen everytime

--- js/cool-load.js
+++ js/cool-load.js
@@ -1,16 +1,37 @@
 $( document ).ready(function() {
-   
 
-      $("#page-top").attr({style: "content:url(img/onload-logo.gif); width:100%; margin: 0 auto;" });
+if(!$.isEmptyObject(localStorage.getItem('isFirstVisit'))){
 
-      $('#page-top').delay(2200).fadeOut(1000, function(){
-         $("#page-top").attr({style: "" });
-         $("#page-top").hide();
-      });
-      $('#page-top').fadeIn(2000);
-     
-   
-  
-   
+var isFirstVisit = JSON.parse(localStorage.getItem('isFirstVisit'));
+
+if (isFirstVisit.first_visit){
+   $("#page-top").attr({style: "content:url(img/onload-logo.gif); width:100%; margin: 0 auto;" });
+
+   $('#page-top').delay(2200).fadeOut(1000, function(){
+      $("#page-top").attr({style: "" });
+      $("#page-top").hide();
+   });
+   $('#page-top').fadeIn(2000);
+   isFirstVisit = { 'first_visit':false };
+// Put the object into storage
+localStorage.setItem('isFirstVisit', JSON.stringify(isFirstVisit));
+}
+
+
+
+}else{
+   $("#page-top").attr({style: "content:url(img/onload-logo.gif); width:100%; margin: 0 auto;" });
+
+   $('#page-top').delay(2200).fadeOut(1000, function(){
+      $("#page-top").attr({style: "" });
+      $("#page-top").hide();
+   });
+   $('#page-top').fadeIn(2000);
+   isFirstVisit = { 'first_visit':false };
+// Put the object into storage
+localStorage.setItem('isFirstVisit', JSON.stringify(isFirstVisit));
+}
+
 });
+
 
