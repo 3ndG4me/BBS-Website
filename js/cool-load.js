80166b3885c2f0cba52850a9d7072b4a43159a94
Casey Erdmann <casey@injecti0n.org>
2017-02-21 20:58:12 -0500

    Update cool-load.js

--- js/cool-load.js
+++ js/cool-load.js
@@ -1,11 +1,6 @@
 $( document ).ready(function() {
 
-   jQuery.browser = {};
-   jQuery.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase());
-   if(jQuery.browser.mozilla){
-      console.log('no animation');  
-      document.getElementById("#main-logo").src= "img/logo1.png";
-   }else{
+
             if(!$.isEmptyObject(localStorage.getItem('isFirstVisit'))){
 
             var isFirstVisit = JSON.parse(localStorage.getItem('isFirstVisit'));
@@ -23,21 +18,7 @@ $( document ).ready(function() {
             localStorage.setItem('isFirstVisit', JSON.stringify(isFirstVisit));
             }
 
-
-
-            }else{
-               $("#page-top").attr({style: "content:url(img/onload-logo.gif); width:100%; margin: 0 auto;" });
-
-               $('#page-top').delay(2200).fadeOut(1000, function(){
-                  $("#page-top").attr({style: "" });
-                  $("#page-top").hide();
-               });
-               $('#page-top').fadeIn(2000);
-               isFirstVisit = { 'first_visit':false };
-            // Put the object into storage
-            localStorage.setItem('isFirstVisit', JSON.stringify(isFirstVisit));
-            }
-   }
+   document.getElementById("#main-logo").src= "img/logo1.png";
 });
 
 
