902ce370549c83d379df2f876bad00a387eb478b
Casey Erdmann <casey@injecti0n.org>
2017-02-21 20:55:00 -0500

    Update cool-load.js

--- js/cool-load.js
+++ js/cool-load.js
@@ -1,37 +1,43 @@
 $( document ).ready(function() {
 
-if(!$.isEmptyObject(localStorage.getItem('isFirstVisit'))){
+   jQuery.browser = {};
+   jQuery.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit    /.test(navigator.userAgent.toLowerCase());
+   if(jQuery.browser.mozilla){
+      console.log('no animation');  
+      document.getElementById("#main-logo").src= "img/logo1.png";
+   }else{
+            if(!$.isEmptyObject(localStorage.getItem('isFirstVisit'))){
 
-var isFirstVisit = JSON.parse(localStorage.getItem('isFirstVisit'));
+            var isFirstVisit = JSON.parse(localStorage.getItem('isFirstVisit'));
 
-if (isFirstVisit.first_visit){
-   $("#page-top").attr({style: "content:url(img/onload-logo.gif); width:100%; margin: 0 auto;" });
+            if (isFirstVisit.first_visit){
+               $("#page-top").attr({style: "content:url(img/onload-logo.gif); width:100%; margin: 0 auto;" });
 
-   $('#page-top').delay(2200).fadeOut(1000, function(){
-      $("#page-top").attr({style: "" });
-      $("#page-top").hide();
-   });
-   $('#page-top').fadeIn(2000);
-   isFirstVisit = { 'first_visit':false };
-// Put the object into storage
-localStorage.setItem('isFirstVisit', JSON.stringify(isFirstVisit));
-}
+               $('#page-top').delay(2200).fadeOut(1000, function(){
+                  $("#page-top").attr({style: "" });
+                  $("#page-top").hide();
+               });
+               $('#page-top').fadeIn(2000);
+               isFirstVisit = { 'first_visit':false };
+            // Put the object into storage
+            localStorage.setItem('isFirstVisit', JSON.stringify(isFirstVisit));
+            }
 
 
 
-}else{
-   $("#page-top").attr({style: "content:url(img/onload-logo.gif); width:100%; margin: 0 auto;" });
+            }else{
+               $("#page-top").attr({style: "content:url(img/onload-logo.gif); width:100%; margin: 0 auto;" });
 
-   $('#page-top').delay(2200).fadeOut(1000, function(){
-      $("#page-top").attr({style: "" });
-      $("#page-top").hide();
-   });
-   $('#page-top').fadeIn(2000);
-   isFirstVisit = { 'first_visit':false };
-// Put the object into storage
-localStorage.setItem('isFirstVisit', JSON.stringify(isFirstVisit));
-}
-
+               $('#page-top').delay(2200).fadeOut(1000, function(){
+                  $("#page-top").attr({style: "" });
+                  $("#page-top").hide();
+               });
+               $('#page-top').fadeIn(2000);
+               isFirstVisit = { 'first_visit':false };
+            // Put the object into storage
+            localStorage.setItem('isFirstVisit', JSON.stringify(isFirstVisit));
+            }
+   }
 });
 
 
